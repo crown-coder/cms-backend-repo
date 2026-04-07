1ae7c66d30b14c316945af5ef60815244a33abe2
APPLE <apple@APPLEs-MacBook-Air.local>
2026-04-07 10:22:03 +0100

    remove path alias and fix imports

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend
+    // origin: "http://localhost:5173", // frontend
+    origin: "https://compliance-management-system-update.netlify.app/", // frontend production
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
