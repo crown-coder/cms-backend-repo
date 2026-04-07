33253722e5d9cee17b654f3147940dcec15e766d
APPLE <apple@APPLEs-MacBook-Air.local>
2026-04-07 10:11:48 +0100

    remove path alias and fix imports

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { db } from "../../config/db";
-import { users, enforcementHeadStates } from "@/db/schema.js";
+import { users, enforcementHeadStates } from "../../db/schema";
 import { eq } from "drizzle-orm";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
--- src/modules/compliance-sections/complianceSections.service.ts
+++ src/modules/compliance-sections/complianceSections.service.ts
@@ -1,5 +1,5 @@
 import { db } from "../../config/db";
-import { complianceSections } from "@/db/schema.js";
+import { complianceSections } from "../../db/schema.js";
 
 export const getSections = async () => {
   return await db.select().from(complianceSections);
