60638feecf29e40713d920209009435c9f396371
APPLE <apple@APPLEs-MacBook-Air.local>
2026-04-14 10:19:47 +0100

    Added PATCH in the Cors Methods

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(
     // origin: "http://localhost:5173", // frontend
     origin: "https://compliance.apps.cac.gov.ng", // frontend production
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   }),
 );
 
